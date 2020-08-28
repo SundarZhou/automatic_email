0ca7e417390c1a56c231e0d390988de36b18b082
sundar_z <sundar.zsd@gmail.com>
2020-08-28 16:40:37 +0800

    fix

--- app/models/repayment_record.rb
+++ app/models/repayment_record.rb
@@ -1,11 +1,11 @@
 class RepaymentRecord < ApplicationRecord
   has_many :credit_card_records
 
-  def message
-    "#{payment_date}-#{total_amount}"
+  def select_message
+    "(#{message})#{payment_date}-#{total_amount}"
   end
 
   def self.pluck_for_select
-    [''] + self.all.map{|r| [r.message, r.id]}
+    [''] + self.all.map{|r| [r.select_message, r.id]}
   end
 end
